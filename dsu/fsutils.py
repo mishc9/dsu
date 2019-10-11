4b0c665cb3832d110ea0465784aedc5de63d8fc9
Mikhail Stepanov <mishc9@gmail.com>
2019-10-11 15:12:43 +0300

    initial code commit

---
+++ dsu/fsutils.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from typing import Union, Iterable
+
+
+def flatten_subdirectories(path: Union[Path, str],
+                           pattern=None,
+                           filter_files_only=True) -> Iterable[Path]:
+    """
+
+    :param path: path to root dir
+    :param pattern: pattern if we need some filtering
+    :param filter_files_only: don't filter directories with pattern if True
+    :return:
+    """
+    path = Path(path)
+    if path.is_dir():
+        if pattern is None:
+            s = [flatten_subdirectories(p) for p in path.iterdir()]
+        else:
+            s = [flatten_subdirectories(p) for p in path.glob(pattern)]
+            if filter_files_only:
+                s_dirs = [flatten_subdirectories(p) for p in path.iterdir() if p.is_dir()]
+                s += s_dirs
+        return [i for x in s for i in x]
+    else:
+        return [path]
+
+
+def make_dir_safely(path: Union[Path, str]) -> None:
+    """
+    Make directory iif it not exists
+    :param path: path to directory
+    :return:
+    """
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != os.errno.EEXIST:
+            raise
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from pathlib import Path
+
+import setuptools
+
+dpath = Path(__file__).parent.absolute()
+
+try:
+    with open(dpath / 'requirements.txt') as f:
+        # read recipe at https://www.reddit.com/r/Python/comments/3uzl2a/setuppy_requirementstxt_or_a_combination/
+        requirements = f.read().splitlines()
+except FileNotFoundError:
+    requirements = list()
+
+setup(name='dsu',
+      version='0.0.1',
+      url='jet.msk.su',
+      license='Apache 2',
+      author='Mikhail Stepanov',
+      author_email='mishc9@gmail.com',
+      description='frequently required utils for ds purposes',
+      packages=setuptools.find_packages(),
+      # dependency_links=["git+https://github.com/mishc9/addata.git@master#egg=addata-0"],
+      include_package_data=True,
+      python_requires='>=3.6.5',
+      # install_requires=requirements + ["addata @ https://github.com/mishc9/addata/tarball/master#egg=addata-0.0.2"],
+      )
