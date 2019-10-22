8f910d48af770615826bd1e022d8a7df4702eac7
Mikhail Stepanov <mishc9@gmail.com>
2019-10-22 14:02:54 +0300

    added new features and fixes, for example, slicing & smart bins

--- dsu/visutils.py
+++ dsu/visutils.py
@@ -1,12 +1,13 @@
+import os
+from math import ceil, floor
 from pathlib import Path
-from typing import Union, Callable, Optional, Tuple
+from typing import Union, Callable, Optional, Tuple, Sized
 
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import pandas as pd
+
 from dsu.fsutils import make_dir_safely
-from math import ceil, floor
 
 
 def bin_width(ser: pd.Series) -> Optional[int]:
@@ -25,7 +26,7 @@ def bin_width(ser: pd.Series) -> Optional[int]:
         return 2 * iqr / quot
 
 
-def n_bins(ser: pd.Series, default: int = 25, max_bins: int = 500) -> int:
+def n_bins(ser: pd.Series, default: int = 25, max_bins: int = 100) -> int:
     """
     Calculate number of bins on histogram via Freedman–Diaconis rule
     :param max_bins: int, max. number of bins
@@ -42,37 +43,66 @@ def n_bins(ser: pd.Series, default: int = 25, max_bins: int = 500) -> int:
     return val if val <= max_bins else max_bins
 
 
-def make_subplots(cols):
+def _get_subplot_grid_params(cols):
     sqrt = floor(cols ** (0.5))
     n_cols = floor(cols / sqrt)
     n_rows = ceil(cols / sqrt)
-
     return n_rows, n_cols
 
 
-def violin(ax, dataframe, col):
+def _get_freq(dataframe: Sized, max_points: int = 25000) -> int:
+    freq = ceil(len(dataframe) / max_points)
+    return freq
+
+
+def violin(ax, dataframe, col, max_points: int = 25000):
     # To slow if print each row, so we're using slices
     # Todo: this function is broken ant don't plot anything
-    return ax.violinplot(dataframe[col].iloc[::100])
+    freq = floor(len(dataframe) / max_points)
+    return ax.violinplot(dataframe[col].iloc[::_get_freq(dataframe, max_points)])
 
 
 def boxplot(ax, dataframe, col):
+    """
+    Do a boxplot
+    :param ax:
+    :param dataframe:
+    :param col:
+    :return:
+    """
     ser: pd.Series = dataframe[col]
     ser: pd.Series = ser[(ser > ser.quantile(q=0.01)) & (ser < ser.quantile(q=0.99))]
     ax.set_title(col)
-    return ser.plot.box(ax=ax)  # n_bins(ser))
+    return ser.plot.box(ax=ax)
 
 
-def hist(ax, dataframe: pd.DataFrame, col):
+def hist(ax, dataframe: pd.DataFrame, col, drop_outliers: bool = True):
+    """
+    Plot histogram
+    :param ax:
+    :param dataframe:
+    :param col:
+    :param drop_outliers:
+    :return:
+    """
     ser: pd.Series = dataframe[col]
-    ser = ser[(ser > ser.quantile(q=0.01)) & (ser < ser.quantile(q=0.99))]
+    if drop_outliers:
+        ser: pd.Series = ser[(ser > ser.quantile(q=0.01)) & (ser < ser.quantile(q=0.99))]
     ax.set_title(col)
-    return ser.hist(ax=ax, bins=25)  # n_bins(ser))
+    return ser.hist(ax=ax, bins=n_bins(ser))
 
 
-def plot(ax, dataframe: pd.DataFrame, col):
-    # To slow if we'll print each row, so we're using slices
-    ser: pd.Series = dataframe[col].iloc[::100]
+def plot(ax, dataframe: pd.DataFrame, col, max_points: int = 25000):
+    """
+    Plot simple curve
+    :param ax:
+    :param dataframe:
+    :param col:
+    :param max_points:
+    :return:
+    """
+    # To slow if we'll print each row, so we use slices
+    ser: pd.Series = dataframe[col].iloc[::_get_freq(dataframe, max_points)]
     return ser.plot(ax=ax)
 
 
@@ -82,18 +112,18 @@ def visualize(dataframe: pd.DataFrame,
               plotters: Tuple = (hist, boxplot, plot,)):
     for plotter in plotters:
         print(f'Going for a {plotter.__name__}')
-        plot_somehow(dataframe, path_to_file, plotter, file_suffix)
+        use_plotter(dataframe, path_to_file, plotter, file_suffix)
 
 
-def plot_somehow(dataframe: pd.DataFrame,
-                 figsave_path: Union[str, Path],
-                 plotter: Callable,
-                 file_suffix=None,
-                 ):
+def use_plotter(dataframe: pd.DataFrame,
+                figsave_path: Union[str, Path],
+                plotter: Callable,
+                file_suffix=None,
+                ):
     cols = dataframe.columns
     col_iter = iter(cols)
 
-    n_rows, n_cols = make_subplots(len(cols))
+    n_rows, n_cols = _get_subplot_grid_params(len(cols))
     fig, axes = plt.subplots(nrows=n_rows,
                              ncols=n_cols,
                              figsize=(25, 25))
