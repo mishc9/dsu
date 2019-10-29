bd6197952757f10f7f23cb538032a76888cc3d56
Mikhail Stepanov <mishc9@gmail.com>
2019-10-29 18:42:21 +0300

    fixed # of subplots bug

--- dsu/vis/utils.py
+++ dsu/vis/utils.py
@@ -46,15 +46,10 @@ def n_bins(series: pd.Series, default: int = 25, max_bins: int = 100) -> int:
 def _get_subplot_grid_params(cols):
     sqrt = ceil(cols ** (0.5))
     n_cols = sqrt
-    n_rows = floor(cols / sqrt)
+    n_rows = ceil(cols / sqrt) if cols % sqrt != 0 else cols // sqrt
     return n_rows, n_cols
 
 
 def _get_freq(dataframe: Sized, max_points: int = 25000) -> int:
     freq = ceil(len(dataframe) / max_points)
     return freq
-
-
-if __name__ == '__main__':
-    for i in range(1, 65):
-        print(_get_subplot_grid_params(i))
